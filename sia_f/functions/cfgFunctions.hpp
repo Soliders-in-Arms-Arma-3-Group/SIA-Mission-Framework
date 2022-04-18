00cd1ddd5df66db18d6f792b32e96bd1fcf4c48b
Liam McKendrick <liammckendirckgaming@gmail.com>
2022-04-17 23:05:01 -1000

    Moved functions folder under sia_f

--- sia_f/functions/cfgFunctions.hpp
+++ sia_f/functions/cfgFunctions.hpp
@@ -2,12 +2,14 @@ class sia_f
 {
 	class briefing
 	{
+		file = "sia_f\functions\briefing";
 		class loadoutNotes {}; // sia_f_fnc_loadoutNotes
 		class orbat {}; // sia_f_fnc_orbat
 	};
 
 	class AFK
 	{
+		file = "sia_f\functions\AFK";
 		class deserializeState {}; // sia_f_fnc_deserializeState
 		class encodeJSON {}; // sia_f_fnc_encodeJSON
 		class exitAFK {}; // sia_f_fnc_exitAFK
@@ -17,16 +19,19 @@ class sia_f
 	
 	class radios
 	{
+		file = "sia_f\functions\radios";
 		class ACRERadioSetup {}; // sia_f_fnc_ACRERadioSetup
 	};
 
 	class safeStart
 	{
+		file = "sia_f\functions\safeStart";
 		class hint {}; // sia_f_fnc_hint
 	};
 
 	class teleport
 	{
+		file = "sia_f\functions\teleport";
 		class teleportToSquad {}; // sia_f_fnc_teleportToSquad
 	};
 };
